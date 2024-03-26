ab3dd16c489ea3ef3c92ef071a995c529931571a
om1007 <117250624+om1007@users.noreply.github.com>
2024-03-26 22:30:24 +0530

    Update door.py

--- door.py
+++ door.py
@@ -1,14 +1,14 @@
 from twilio.rest import Client
 
-account_sid = 'ACceade456cb4bd327e9944d06365436c8'
-auth_token = '3c6af0a79acbd83576e3527c59ca1aa3'
+account_sid = ''
+auth_token = ''
 client = Client(account_sid, auth_token)
 
 def sendSms():
     message = client.messages.create(
-    from_='+14433414337',
+    from_='',
     body='Alert : Unknown Detected !!',
-    to='+918779823783'
+    to=''
     )
 
     print(message.sid)
